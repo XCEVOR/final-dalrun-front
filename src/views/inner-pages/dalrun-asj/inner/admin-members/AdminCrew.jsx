fa3f534eda95eb2ffac8c0c46469306ed9b53a17
AnSeonJeong <asj081@naver.com>
2023-04-14 14:40:46 +0900

    style : 회원관리 - 개인회원/크루 ui, css수정

--- src/views/inner-pages/dalrun-asj/inner/admin-members/AdminCrew.jsx
+++ src/views/inner-pages/dalrun-asj/inner/admin-members/AdminCrew.jsx
@@ -1,5 +1,12 @@
 function AdminCrew() {
-  return <div></div>;
+  return (
+    <div className="member">
+      <div className="member-content">
+        <div className="search outline"></div>
+        <div className="info outline">크루</div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminCrew;
--- src/views/inner-pages/dalrun-asj/inner/admin-members/AdminMember.jsx
+++ src/views/inner-pages/dalrun-asj/inner/admin-members/AdminMember.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 function AdminMember() {
   return (
-    <div className="member container">
-      <h4 className="title">회원관리</h4>
-      <button>개인회원</button>
-      <button>크루</button>
+    <div className="member">
       <div className="member-content">
         <div className="search outline"></div>
-        <div className="member-info outline"></div>
+        <div className="info outline">개인회원</div>
       </div>
     </div>
   );
