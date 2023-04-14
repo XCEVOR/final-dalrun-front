923d75d67c5b819d2e74bf5826817459c1dbfe62
AnSeonJeong <asj081@naver.com>
2023-04-14 13:45:57 +0900

    style : 회원관리 ui

--- src/views/inner-pages/dalrun-asj/inner/admin-members/AdminMember.jsx
+++ src/views/inner-pages/dalrun-asj/inner/admin-members/AdminMember.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+
 function AdminMember() {
-  return <div></div>;
+  return (
+    <div className="member container">
+      <h4 className="title">회원관리</h4>
+      <button>개인회원</button>
+      <button>크루</button>
+      <div className="member-content">
+        <div className="search outline"></div>
+        <div className="member-info outline"></div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminMember;
