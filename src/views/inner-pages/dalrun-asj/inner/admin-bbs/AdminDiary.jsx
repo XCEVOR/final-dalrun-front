c6129086031628bf8b95182ea1b70df5587f1f1e
AnSeonJeong <asj081@naver.com>
2023-04-14 17:17:05 +0900

    style : 게시물관리(문의,리뷰,다이어리,대회일정) ui & css

--- src/views/inner-pages/dalrun-asj/inner/admin-bbs/AdminDiary.jsx
+++ src/views/inner-pages/dalrun-asj/inner/admin-bbs/AdminDiary.jsx
@@ -1,5 +1,12 @@
 function AdminDiary() {
-  return <div></div>;
+  return (
+    <div className="bbs">
+      <div className="bbs-content">
+        <div className="search outline"></div>
+        <div className="info outline">다이어리</div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminDiary;
