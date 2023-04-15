b91327aef99e16813049e64490e21859ffe8b692
Subak-Uncle <kko6369@naver.com>
2023-04-15 14:41:36 +0900

    feat:페이지 로고 및 사이트 이름 변경

--- src/views/inner-pages/dalrun-kjw/Diary.jsx
+++ src/views/inner-pages/dalrun-kjw/Diary.jsx
@@ -4,8 +4,15 @@ import KakaoMap from '../../../components/dalrun-jw/KakaoMap';
 function Diary(){
   return(
     <div className='row'>
-      <div className='col-md-2'>막대</div>
-      <div className='col-md-10'>
+      <div className='col-md-2'>
+        <div className='d-flex flex-colum flex-shrink-0 gb-body-tertiary ' data-bs-theme="dark" style={{ width:"4.5rem"}}>
+          <ul className="nav nav-pills nav-flush felx-column mb-auto text-cneter">
+            <li className='nav-item'></li>
+          </ul>          
+        </div>
+      </div>
+      <div className='col-md-1'>막대</div>
+      <div className='col-md-9'>
         <div className='row'>< KakaoMap/></div>
         <div className='row' style={{textAlign:"center"}}>기록 분석</div>
       </div>
