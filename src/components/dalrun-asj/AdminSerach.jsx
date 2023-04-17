46f5500a5fb10ba91a3cf86f99c2d4e60c37329f
AnSeonJeong <asj081@naver.com>
2023-04-17 17:39:52 +0900

    feat : 검색기능 구현

--- src/components/dalrun-asj/AdminSerach.jsx
+++ src/components/dalrun-asj/AdminSerach.jsx
@@ -1,19 +1,53 @@
-import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 function AdminSearch() {
     const [searchParams, setSearchParams] = useSearchParams();
+    const [grade, setGrade] = useState("");
+    const [date, setDate] = useState("");
+    const [stockState, setStockState] = useState("");
     
+    const { cate } = useParams();
     const choice = searchParams.get("choice");
     const search = searchParams.get("search");
-    const grade = searchParams.get("grade");
 
-    console.log(choice, search, grade);
+    const [params, setParams] = useState({"choice" : choice, "search" : search});
+    const [dataList, setDateList] = useState([]);
 
-    const getDataList = () => {}
+    const Search = () => {
+        if(choice === null || search === null) return;
+    
+        if(cate === "member") {
+            setGrade(searchParams.get("grade"));
+            setParams({...params, "grade":grade});
+        } else if(cate === "competition") {
+            setDate(searchParams.get("date"));
+            setParams({...params, "date":date});
+        } else if(cate === "stock") {
+            setStockState(searchParams.get("state"));
+            setParams({...params, "state":stockState});
+        }
+    }
 
-    return (
-        <div></div>
-    );
+    const getDataList = () => {
+        axios.get(`http://localhost:3000/${cate}`, { params: params })
+             .then((resp) => {
+                console.log(resp);
+                // 검색결과리스트 dataList에 저장 -> 관리자 게시판 컴포넌트에 전달
+             })
+             .catch((err) => {
+                console.log(err);
+             });
+    }
+
+    // url이 바뀔 때만 렌더링
+    useEffect(() => {
+        Search();
+        // getDataList();
+    },[searchParams]);
+
+    return;
 }
 
 export default AdminSearch;
