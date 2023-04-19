8dd86846479a517fc09da14a074831bcdcdb5e66
AnSeonJeong <asj081@naver.com>
2023-04-19 17:35:39 +0900

    feat : 검색 적용(대회일정, 주문관리)

--- src/views/inner-pages/dalrun-asj/inner/admin-bbs/AdminCompetition.jsx
+++ src/views/inner-pages/dalrun-asj/inner/admin-bbs/AdminCompetition.jsx
@@ -1,8 +1,17 @@
+import BasicSearch from "../../../../../components/dalrun-asj/BasicSearch";
+
 function AdminCompetition() {
+  const optionVal = [
+    {value : "seq", name : "대회번호"},
+    {value : "name", name : "대회명"},
+    {value : "location", name : "대회지역"},
+    {value : "content", name : "대회내용"},
+  ]
+
   return (
     <div className="bbs">
       <div className="bbs-content">
-        <div className="search outline"></div>
+        <BasicSearch {...optionVal} />
         <div className="info outline">대회일정</div>
       </div>
     </div>
