483069d0302b6e3dee1e38716458ecfff26fc8e4
Subak-Uncle <kko6369@naver.com>
2023-04-20 22:07:28 +0900

    docs: 아이디 추가

--- src/views/inner-pages/dalrun-kjw/Diary.jsx
+++ src/views/inner-pages/dalrun-kjw/Diary.jsx
@@ -1,26 +1,26 @@
 ﻿import React from "react";
 import KakaoMap from '../../../components/dalrun-jw/KakaoMap';
-import NaverMap from '../../../components/dalrun-jw/NaverMapApi';
+import NaverMapApi from '../../../components/dalrun-jw/NaverMapApi';
 import DiarySidebar from '../../../components/dalrun-jw/diary/DiarySidebar';
 import DiaryList from '../../../components/dalrun-jw/diary/DiaryList';
-import "bootstrap/dist/css/bootstrap.css";
-import NaverMapApi from '../../../components/dalrun-jw/NaverMapApi';
+
 
 function Diary(){
   return(
     <div className='row'>
 
       {/* 사이드 바 */}
-      <div className='col-md-1 min-vh-100 bg-dark'>
+      <div id="sideNav" className='col-md-1 min-vh-100 bg-dark'>
         < DiarySidebar/>
       </div>
 
       {/* 기록 리스트 */}
-      <div className='col-md-2'>
+      <div id="list" className='col-md-2'>
         < DiaryList />
       </div>
+
       {/* 지도 부분 */}
-      <div className='col-md-9'>< NaverMapApi/></div>
+      <div id="map" className='col-md-9'>< NaverMapApi/></div>
     </div>
     )
 }
