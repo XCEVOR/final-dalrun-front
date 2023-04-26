077eb8ffcbbf08576a1942d1010472e2f9505fbd
XCEVOR <XCEVOR@GMAIL.COM>
2023-04-26 16:19:39 +0900

    feat: db cart 에 저장된 productid & memid 가져옴

--- src/components/dalrun-hc/storecart/StoreCartList.jsx
+++ src/components/dalrun-hc/storecart/StoreCartList.jsx
@@ -1,121 +1,189 @@
-import React, { setState, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-// import "./styles.css";
 
 function StoreCartList() {
   const location = useLocation();
+  const [userId, setUserId] = useState("");
+  const [cartList, setCartList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const [likeBtn, setLikeBtn] = useState(false);
   console.log("console.log(location.state); ", location.state);
+
+
+  const getCartList = async (productCode) => {
+    const resp = await axios.post("http://localhost:3000/getCartList", null, { params: {"memId": userId} });
+    console.log("getProductData: ", resp.data);
+    setCartList(resp.data);
+
+    setLoading(true);  // 이 코드 전에는 div에 productDetails.productName 등등 적용안됨.
+  }
+
+  useEffect(() => {
+    getCartList();
+  }, [])
+
+  if(loading === false){
+    return <div>Loading...</div>
+  }
+
+
+
+
 
   const likeBtnClick = () => {
     setLikeBtn(!likeBtn);
   };
 
   return (
-    <div className="App">
+    <div>
       <h1>CART</h1>
-      <div class="shopping-cart">
+      {/* <div className="shopping-cart"> */}
+      <div>
         {/* <!-- Title --> */}
-        <div class="title">Shopping Bag</div>
+        <div className="title">Shopping Bag</div>
 
         {/* <!-- Product #1 --> */}
-        <div class="item">
-          <div class="buttons">
-            <span class="delete-btn"></span>
+        <div className="item">
+          <div className="buttons">
+            <span className="delete-btn"></span>
             <span
               className={likeBtn ? "like-btn is-active" : "like-btn"}
               onClick={likeBtnClick}
             ></span>
           </div>
 
-          <div class="image">
+          <div className="image">
             <img src="assets/img/dalrun-hc/store/storecart/item-1.png" alt="" />
           </div>
 
-          <div class="description">
+          <div className="description">
             <span>Common Projects</span>
             <span>Bball High</span>
             <span>White</span>
           </div>
 
-          <div class="quantity">
-            <button class="plus-btn" type="button" name="button">
+          <div className="quantity">
+            <button className="plus-btn" type="button" name="button">
               <img src="assets/img/dalrun-hc/store/storecart/plus.svg" alt="" />
             </button>
             <input type="text" name="name" value="1" />
-            <button class="minus-btn" type="button" name="button">
+            <button className="minus-btn" type="button" name="button">
               <img src="assets/img/dalrun-hc/store/storecart/minus.svg" alt="" />
             </button>
           </div>
 
-          <div class="total-price">$549</div>
+          <div className="total-price">$549</div>
         </div>
 
         {/* <!-- Product #2 --> */}
-        <div class="item">
-          <div class="buttons">
-            <span class="delete-btn"></span>
+        <div className="item">
+          <div className="buttons">
+            <span className="delete-btn"></span>
             <span
               className={likeBtn ? "like-btn is-active" : "like-btn"}
               onClick={likeBtnClick}
             ></span>
           </div>
 
-          <div class="image">
+          <div className="image">
             <img src="assets/img/dalrun-hc/store/storecart/item-2.png" alt="" />
           </div>
 
-          <div class="description">
+          <div className="description">
             <span>Maison Margiela</span>
             <span>Future Sneakers</span>
             <span>White</span>
           </div>
 
-          <div class="quantity">
-            <button class="plus-btn" type="button" name="button">
+          <div className="quantity">
+            <button className="plus-btn" type="button" name="button">
               <img src="assets/img/dalrun-hc/store/storecart/plus.svg" alt="" />
             </button>
             <input type="text" name="name" value="1" />
-            <button class="minus-btn" type="button" name="button">
+            <button className="minus-btn" type="button" name="button">
               <img src="assets/img/dalrun-hc/store/storecart/minus.svg" alt="" />
             </button>
           </div>
 
-          <div class="total-price">$870</div>
+          <div className="total-price">$870</div>
         </div>
 
         {/* <!-- Product #3 --> */}
-        <div class="item">
-          <div class="buttons">
-            <span class="delete-btn"></span>
+        <div className="item">
+          <div className="buttons">
+            <span className="delete-btn"></span>
             <span
               className={likeBtn ? "like-btn is-active" : "like-btn"}
               onClick={likeBtnClick}
             ></span>
           </div>
 
-          <div class="image">
+          <div className="image">
             <img src="assets/img/dalrun-hc/store/storecart/item-3.png" alt="" />
           </div>
 
-          <div class="description">
+          <div className="description">
             <span>Our Legacy</span>
             <span>Brushed Scarf</span>
             <span>Brown</span>
           </div>
 
-          <div class="quantity">
-            <button class="plus-btn" type="button" name="button">
+          <div className="quantity">
+            <button className="plus-btn" type="button" name="button">
               <img src="assets/img/dalrun-hc/store/storecart/plus.svg" alt="" />
             </button>
             <input type="text" name="name" value="1" />
-            <button class="minus-btn" type="button" name="button">
+            <button className="minus-btn" type="button" name="button">
               <img src="assets/img/dalrun-hc/store/storecart/minus.svg" alt="" />
             </button>
           </div>
 
-          <div class="total-price">$349</div>
+          <div className="total-price">$349</div>
         </div>
+
+
+
+        {/* <!-- DB 데이터 --> */}
+        {cartList.map((item, index) => (
+          <div className="item">
+            <div className="buttons">
+              <span className="delete-btn"></span>
+              <span
+                className={likeBtn ? "like-btn is-active" : "like-btn"}
+                onClick={likeBtnClick}
+              ></span>
+            </div>
+
+            <div className="image">
+              <img src="assets/img/dalrun-hc/store/storecart/item-3.png" alt="" />
+            </div>
+
+            <div className="description">
+              <span> {item.productId }</span>
+              <span>Brushed Scarf</span>
+              <span>Brown</span>
+            </div>
+
+            <div className="quantity">
+              <button className="plus-btn" type="button" name="button">
+                <img src="assets/img/dalrun-hc/store/storecart/plus.svg" alt="" />
+              </button>
+              <input type="text" name="name" value={ item.cartProdQuantity } />
+              <button className="minus-btn" type="button" name="button">
+                <img src="assets/img/dalrun-hc/store/storecart/minus.svg" alt="" />
+              </button>
+            </div>
+
+            <div className="total-price">$349</div>
+          </div>
+        ))}
+
+
+    
       </div>
     </div>
   );
