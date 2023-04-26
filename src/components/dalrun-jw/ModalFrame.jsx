c037a2a9b0be815350c85bf66e511b8decaa11b8
Subak-Uncle <kko6369@naver.com>
2023-04-26 09:56:00 +0900

    feat: 다이어리 업로드 modal 생성

---
+++ src/components/dalrun-jw/ModalFrame.jsx
@@ -0,0 +1,31 @@
+﻿import React, { useState } from 'react';
+
+const ModalFrame = (props) => {
+
+  const { open, close, header } = props;
+
+  return(
+
+    // 모달이 열릴때 openModal 클래스가 생성된다.
+    <div className={open ? 'openModal modal' : 'modal'}>
+    {open ? (
+      <section>
+        <header>
+          {header}
+          <button className="close" onClick={close}>
+            &times;
+          </button>
+        </header>
+        <main>{props.children}</main>
+        <footer>
+          <button className="close" onClick={close}>
+            close
+          </button>
+        </footer>
+      </section>
+    ) : null}
+    </div>
+  );
+}
+
+export default ModalFrame;
