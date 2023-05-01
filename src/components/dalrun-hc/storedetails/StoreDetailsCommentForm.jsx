2592916f954b839643630cf804d5df69e9cc18fd
XCEVOR <XCEVOR@GMAIL.COM>
2023-05-01 13:57:38 +0900

    test: 스토어 코멘트 반영 기능 테스트 (코드 정리X)

--- src/components/dalrun-hc/storedetails/StoreDetailsCommentForm.jsx
+++ src/components/dalrun-hc/storedetails/StoreDetailsCommentForm.jsx
@@ -9,7 +9,6 @@ import TestReduxLeft from "./StoreDetailsCommentList";  // TEST REDUX
 import { configureStore, createSlice } from '@reduxjs/toolkit';  // TEST REDUX
 
 
-
 function StoreDetailsCommentForm() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -49,8 +48,10 @@ function StoreDetailsCommentForm() {
         <div>
             <Provider store={myReduxStore}>
                 {/* <TestReduxLeft></TestReduxLeft> */}{/*  // TEST REDUX*/}
-                <TestReduxRight></TestReduxRight>
+                {/* <TestReduxRight></TestReduxRight> */}
+                <TestReduxRight2></TestReduxRight2>
             </Provider>
+        {/*             
         <div className="post-comment-form">
             <h4>Leave a Reply </h4>
             <span>Your email address will not be published.</span>
@@ -85,7 +86,9 @@ function StoreDetailsCommentForm() {
                     </div>
                 </form>
             </div>
-        </div>
+        </div> 
+        */}
+
         </div>
     )
 }
@@ -133,7 +136,7 @@ function StoreDetailsCommentForm() {
 
 ////////// ////////// ////////// ////////// ////////// 
 // ===> ./StoreDetailsCommentForm.jsx
-function TestReduxRight () {
+function TestReduxRight () {    const [name, setName] = useState('');
 
     const myDispatch = useDispatch();
 
@@ -145,6 +148,110 @@ function TestReduxRight () {
     )
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////// ////////// ////////// ////////// ////////// 
+// ===> COMMENT
+function TestReduxRight2 () {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+    const [productId, setProductId] = useState('TestProductId');
+    const [memId, setMemId] = useState('TestMemId');
+
+    const myDispatch = useDispatch();
+
+    const writeComment = () => {
+    if(subject === undefined || subject.trim() === ''){
+        alert('제목을 입력해 주십시오');
+        return;
+    }
+
+    axios
+      .post("http://localhost:3000/writeProductInquiry", null, {
+        params: {
+          inqWriter: name,
+          inqContent: message,
+          productId: productId,
+          memId: memId,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data === "SUCCESS") {
+          alert("성공적으로 등록되었습니다");
+          // history('/bbslist');    // bbslist로 이동
+        } else {
+          alert("등록되지 않았습니다");
+        }
+      })
+      .catch(function (err) {
+        alert(err);
+      });   
+    }
+
+    const myOnClickFunc = () => {
+        writeComment();
+        myDispatch( {type: "myCounterInSlice/PLUS", step: 2} );
+    }
+
+
+
+    return (
+        <div>
+            <h1>TEST REDUX RIGHT</h1>
+            <input type="button" value="  // TEST REDUX +2" onClick={() => myDispatch( {type: "myCounterInSlice/PLUS", step: 2} )}></input>
+
+            <div className="post-comment-form">
+            <h4>Leave a Reply </h4>
+            <span>Your email address will not be published.</span>
+            <div className="bd-contact-form-wrapper mb-30">
+                <form action="#">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="bd-contact-field mb-30">
+                                <input type="text" placeholder="Name"  value={name}  onChange={(e)=>setName(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="bd-contact-field mb-30">
+                                <input type="email" placeholder="Email"  value={email}  onChange={(e)=>setEmail(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="bd-contact-field mb-30">
+                                <input type="text" placeholder="Subject"  value={subject}  onChange={(e)=>setSubject(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="bd-contact-field mb-30">
+                                <textarea placeholder="Message"  value={message}  onChange={(e)=>setMessage(e.target.value)}></textarea>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="bd-contact-field">
+                                <button type="submit" className="theme-btn" onClick={myOnClickFunc}>Submit test</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        </div>
+    )
+}
 
 
 export default StoreDetailsCommentForm;
--- src/components/dalrun-hc/storedetails/StoreDetailsCommentList.jsx
+++ src/components/dalrun-hc/storedetails/StoreDetailsCommentList.jsx
@@ -19,6 +19,7 @@ function StoreDetailsCommentList() {
 
     const [inquiryList, setInquiryList] = useState([]);
 
+
     const getCommentList = (productCode) => {
         axios.post("http://localhost:3000/getProductInquiry", null, 
                     { params:{ "productCode": productCode } })
@@ -49,85 +50,88 @@ function StoreDetailsCommentList() {
 
     return (
         <div>
-            <Provider store={myReduxStore}>
-                <TestReduxLeft></TestReduxLeft>
-            </Provider>
-        <div className="post-comments mb-95">
-        <div className="post-comment-title mb-40">
-            <h3>3 Comments</h3>
-        </div>
-        <div className="latest-comments">
-            <ul>
-                <li>
-                    <div className="comments-box">
-                        <div className="comments-avatar">
-                            <img src="assets/img/blog/blog-sm-6.png" className="img-fluid" alt="img"/>
-                        </div>
-                        <div className="comments-text">
-                            <div className="avatar-name">
-                                <h5>프론트: David Angel Makel</h5>
-                                <span className="post-meta">February 26, 2022</span>
-                            </div>
-                            <p>The bee's knees bite your arm off bits and bobs he nicked it gosh gutted mate blimey, old off his nut argy bargy vagabond buggered dropped.</p>
-                            <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
-                        </div>
-                    </div>
-                </li>
-                <li className="children">
-                    <div className="comments-box">
-                        <div className="comments-avatar">
-                            <img src="assets/img/blog/blog-sm-7.png" className="img-fluid" alt="img"/>
-                        </div>
-                        <div className="comments-text">
-                            <div className="avatar-name">
-                                <h5>프론트: Bailey Wonger</h5>
-                                <span className="post-meta">February 20, 2022</span>
-                            </div>
-                            <p>Do one say wind up buggered bobby bite your arm off gutted mate, David victoria sponge cup of char chap fanny around.</p>
-                            <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
-                        </div>
-                    </div>
-                </li>
-                <li className="children">
-                    <div className="comments-box">
-                        <div className="comments-avatar">
-                            <img src="assets/img/blog/blog-sm-8.png" className="img-fluid" alt="img"/>
-                        </div>
-                        <div className="comments-text">
-                            <div className="avatar-name">
-                                <h5>프론트: Hilary Ouse</h5>
-                                <span className="post-meta">February 21, 2022</span>
-                            </div>
-                            <p>Baking cakes is cobblers wellies William geeza bits and bobs what a plonker it's your round,</p>
-                            <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
 
-        {inquiryList.map((inq, index) => (
-        <div className="latest-comments" key={index}>
-            <ul>
-                <li>
-                    <div className="comments-box">
-                        <div className="comments-avatar">
-                            <img src="assets/img/blog/blog-sm-6.png" className="img-fluid" alt="img"/>
-                        </div>
-                        <div className="comments-text">
-                            <div className="avatar-name">
-                                <h5>서버: {inq.inqWriter}</h5>
-                                <span className="post-meta">{inq.inqDate}</span>
+        <div className="post-comments mb-95">
+            <div className="post-comment-title mb-40">
+                <h3>3 Comments</h3>
+            </div>
+            <div className="latest-comments">
+                <ul>
+                    <li>
+                        <div className="comments-box">
+                            <div className="comments-avatar">
+                                <img src="assets/img/blog/blog-sm-6.png" className="img-fluid" alt="img"/>
                             </div>
-                            <p>{inq.inqContent}</p>
-                            <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
+                            <div className="comments-text">
+                                <div className="avatar-name">
+                                    <h5>프론트: David Angel Makel</h5>
+                                    <span className="post-meta">February 26, 2022</span>
+                                </div>
+                                <p>The bee's knees bite your arm off bits and bobs he nicked it gosh gutted mate blimey, old off his nut argy bargy vagabond buggered dropped.</p>
+                                <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
+                            </div>
                         </div>
-                    </div>
-                </li>
-            </ul>
+                    </li>
+                    <li className="children">
+                        <div className="comments-box">
+                            <div className="comments-avatar">
+                                <img src="assets/img/blog/blog-sm-7.png" className="img-fluid" alt="img"/>
+                            </div>
+                            <div className="comments-text">
+                                <div className="avatar-name">
+                                    <h5>프론트: Bailey Wonger</h5>
+                                    <span className="post-meta">February 20, 2022</span>
+                                </div>
+                                <p>Do one say wind up buggered bobby bite your arm off gutted mate, David victoria sponge cup of char chap fanny around.</p>
+                                <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="children">
+                        <div className="comments-box">
+                            <div className="comments-avatar">
+                                <img src="assets/img/blog/blog-sm-8.png" className="img-fluid" alt="img"/>
+                            </div>
+                            <div className="comments-text">
+                                <div className="avatar-name">
+                                    <h5>프론트: Hilary Ouse</h5>
+                                    <span className="post-meta">February 21, 2022</span>
+                                </div>
+                                <p>Baking cakes is cobblers wellies William geeza bits and bobs what a plonker it's your round,</p>
+                                <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            {/* 
+            {inquiryList.map((inq, index) => (
+            <div className="latest-comments" key={index}>
+                <ul>
+                    <li>
+                        <div className="comments-box">
+                            <div className="comments-avatar">
+                                <img src="assets/img/blog/blog-sm-6.png" className="img-fluid" alt="img"/>
+                            </div>
+                            <div className="comments-text">
+                                <div className="avatar-name">
+                                    <h5>서버: {inq.inqWriter}</h5>
+                                    <span className="post-meta">{inq.inqDate}</span>
+                                </div>
+                                <p>{inq.inqContent}</p>
+                                <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            ))}
+            */}
+            <Provider store={myReduxStore}>
+                {/* <TestReduxLeft></TestReduxLeft> */}
+                <TestReduxLeft2></TestReduxLeft2>
+            </Provider>
         </div>
-        ))}
-    </div>
     </div>
     )
 
@@ -143,6 +147,80 @@ function TestReduxLeft () {
     return (
         <div>
             <h1>TEST REDUX LEFT: {number}</h1>
+        </div>
+    )
+}
+
+function TestReduxLeft2 () {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+    const [productId, setProductId] = useState('TestProductId');
+    const [memId, setMemId] = useState('TestMemId');
+    const [productCode, setProductCode] = useState('Test_ProductCode');
+
+    const [loading, setLoading] = useState(false);
+
+    const [inquiryList, setInquiryList] = useState([]);
+
+    const number = useSelector(state => state.myCounterInConfigureStore.number)
+
+
+    const getCommentList = (productCode) => {
+        axios.post("http://localhost:3000/getProductInquiry", null, 
+                    { params:{ "productCode": productCode } })
+             .then(res => {
+                console.log(res.data);
+                setInquiryList(res.data);
+                // if(res.data === "SUCCESS"){
+                //     alert("성공적으로 등록되었습니다");
+                //     // history('/bbslist');    // bbslist로 이동
+                // }else{
+                //     alert("등록되지 않았습니다");
+                // }
+                setLoading(true);  // 이 코드 전에는 div에 productDetails.productName 등등 적용안됨.
+             })
+             .catch(function(err){
+                alert(err);
+             })   
+    }
+
+    useEffect(() => {
+        getCommentList(productCode);
+      }, [productCode, number])
+    
+      if(loading === false){
+        return <div>Loading...</div>
+      }
+
+
+    return (
+        <div>
+            <h1>TEST REDUX LEFT: {number}</h1>
+            
+            {inquiryList.map((inq, index) => (
+            <div className="latest-comments" key={index}>
+                <ul>
+                    <li>
+                        <div className="comments-box">
+                            <div className="comments-avatar">
+                                <img src="assets/img/blog/blog-sm-6.png" className="img-fluid" alt="img"/>
+                            </div>
+                            <div className="comments-text">
+                                <div className="avatar-name">
+                                    <h5>서버22: {inq.inqWriter}</h5>
+                                    <span className="post-meta">{inq.inqDate}</span>
+                                </div>
+                                <p>{inq.inqContent}</p>
+                                <a href="#" className="comment-reply"><i className="fal fa-reply"></i> Reply</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            ))}
+            
         </div>
     )
 }
