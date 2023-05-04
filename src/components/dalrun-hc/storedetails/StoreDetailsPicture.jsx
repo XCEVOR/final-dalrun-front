39fc574904a444605883bb9ae5391b568cbc2ed0
XCEVOR <XCEVOR@GMAIL.COM>
2023-05-04 23:01:13 +0900

    0504.1100-store-detail-display-mode-test

--- src/components/dalrun-hc/storedetails/StoreDetailsPicture.jsx
+++ src/components/dalrun-hc/storedetails/StoreDetailsPicture.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 function StoreDetailsPicture() {
   let prodParams = useParams();
   console.log("StoreDetailsPicture() {: ", prodParams.productCode);
+  const [checkbox_DisplayMode, setCheckbox_DisplayMode] = useState(true);  // TEST MODE
 
   const [productPictureList, setProductPictureList] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -27,7 +28,11 @@ function StoreDetailsPicture() {
   }
 
 
-  return (
+  return checkbox_DisplayMode 
+  // USER_MODE
+  ? (
+    <>
+    <input type='checkbox' onClick={() =>(setCheckbox_DisplayMode(!checkbox_DisplayMode))}/>USER_MODE
     <div>
       <div className="product_productOrigFile">
         <h1 className="product_productOrigFile">product_origfile_blob 서버: {productPictureList[0]}</h1>
@@ -46,7 +51,34 @@ function StoreDetailsPicture() {
 
 
     </div>
-  );
+    </>
+    )
+
+
+    // DEVELOPER_MODE
+    : (
+      <>
+      <input type='checkbox' onClick={() => (setCheckbox_DisplayMode(!checkbox_DisplayMode))}/>DEVELOPER_MODE
+      <div>
+      <div className="product_productOrigFile">
+        <h1 className="product_productOrigFile">product_origfile_blob 서버: {productPictureList[0]}</h1>
+      </div>
+
+      {productPictureList.map((pic, index) => (
+        <div key={index}>
+          <p>{index}</p>
+          <img
+            src={`http://localhost:3000/dalrun-hc/store/products/${prodParams.productCode}/${pic}`}
+            alt={pic}
+            loading="lazy"
+          />
+        </div>
+      ))}
+
+
+    </div>
+    </>
+    )
 }
 
 export default StoreDetailsPicture;
--- src/components/dalrun-hc/storedetails/StoreDetailsSelection.jsx
+++ src/components/dalrun-hc/storedetails/StoreDetailsSelection.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 function StoreDetailsSelection() {
     let prodParams = useParams();
     console.log(prodParams.productCode);
+    const [checkbox_DisplayMode, setCheckbox_DisplayMode] = useState(true);  // TEST MODE
   
     const [productDetails, setProductDetails] = useState();
     const [itemColorList, setItemColorList] = useState([]);
@@ -118,8 +119,14 @@ function StoreDetailsSelection() {
 
 
 
-    return (
+    return checkbox_DisplayMode 
+    // USER_MODE
+    ? (
+      <>
+      <input type='checkbox' onClick={() =>(setCheckbox_DisplayMode(!checkbox_DisplayMode))}/>USER_MODE
       <div>
+
+        
         <button onClick={updateProductView}>VIEW TEST</button>
         <button onClick={updateProductLike}>LIKE TEST</button>
         <button value="1" onClick={(e) => updateProductRecomm(e.target.value)}>RECOMM 1 TEST</button>
@@ -217,7 +224,116 @@ function StoreDetailsSelection() {
           <h1 className="product_description">product_origfile_blob 서버: {productDetails[0].productOrigFile}</h1>
         </div>
       </div>
-    );
+      </>
+    )
+
+
+    // DEVELOPER_MODE
+    : (
+      <>
+      <input type='checkbox' onClick={() => (setCheckbox_DisplayMode(!checkbox_DisplayMode))}/>DEVELOPER_MODE
+      <div>
+
+        
+        <button onClick={updateProductView}>VIEW TEST</button>
+        <button onClick={updateProductLike}>LIKE TEST</button>
+        <button value="1" onClick={(e) => updateProductRecomm(e.target.value)}>RECOMM 1 TEST</button>
+        <button value="2" onClick={(e) => updateProductRecomm(e.target.value)}>RECOMM 2 TEST</button>
+        <button value="3" onClick={(e) => updateProductRecomm(e.target.value)}>RECOMM 3 TEST</button>
+
+        
+        <div className="product_id">
+          <h1 className="product_id">product_id 서버: {productDetails[0].productId}</h1>
+        </div>
+        <div className="product_code">
+          <h1 className="product_code">product_code 서버: {productDetails[0].productCode}</h1>
+        </div>
+        <div className="product_category">
+          <h1 className="product_category">product_category 서버: {productDetails[0].productCategory}</h1>
+        </div>
+        <div className="product_name">
+          <h1 className="product_name">product_name 서버: {productDetails[0].productName}</h1>
+        </div>
+        <div className="product_brand">
+          <h1 className="product_brand">product_brand 서버: {productDetails[0].productBrand}</h1>
+        </div>
+        <div className="product_price">
+          <h1 className="product_price">product_price 서버: {productDetails[0].productPrice}</h1>
+        </div>
+        <div className="product_color">
+          <button className="">f-red</button>
+          <button className="">f-blue</button>
+          <button className="">f-green</button>
+          {itemColorList.map((icolor, index) => (
+            <button className="product_color" key={index} value={icolor} onClick={selectColorBtn}>{icolor}</button>
+          ))}
+        </div>
+        <div className="product_size">
+          <button className="product_size">f-210</button>
+          <button className="product_size">f-220</button>
+          {itemSizeList.map((isize, index) => (
+            <button className="product_size" key={index} value={isize} onClick={selectSizeBtn}>{isize}</button>
+          ))}
+        </div>
+        <div className="product_quantity">
+          <button onClick={(e) => setSelectedQuantity(prevQuantity => prevQuantity + 1)}>+</button>
+          <p>{selectedQuantity}</p>
+          <button onClick={selectQuantityMinusBtn}>-</button>
+        </div>
+
+        <div className="product_quantity">
+          <p>{selectedColor}//</p>
+          <p>{selectedSize}//</p>
+          <p>{selectedQuantity}</p>
+        </div>
+
+        <div>
+          <button onClick={selectProductOptionTest}>상품 ID 체크 테스트</button>
+          <p>
+            {selectedItemInfo[0].productId}//
+            {selectedItemInfo[0].productCode}//
+            {selectedItemInfo[0].productColor}//
+            {selectedItemInfo[0].productSize}//
+            {selectedProdId}//
+          </p>
+        </div>
+
+        <div className="product_cart">
+            <button onClick={addToCart}>ADD TO CART</button>
+        </div>
+        <div className="product_cart">
+          <Link to="/store-cart" state={{ "orderProductId": userOrderData.orderProductId, "orderProductQuantity": userOrderData.orderProductQuantity }}>
+            <button onClick={addToCart}>ADD TO CART & GO</button>
+          </Link>
+        </div>
+        <Link
+          className="ptf-btn ptf-btn--primary ptf-btn--block"
+          to="/store-cart"
+        >
+          장바구니
+            <p>{userOrderData.productId}//ID: {selectedProdId}//Qty: {userOrderData.orderProductQuantity}</p>
+        </Link>
+
+        <div className="product_checkout">
+          <button>CHECKOUT</button>
+
+        </div>
+        <Link
+          className="ptf-btn ptf-btn--primary ptf-btn--block"
+          to="/store-payment"
+        >
+          바로구매
+        </Link>
+
+        <div className="product_description">
+          <h1 className="product_description">product_description 서버: {productDetails[0].productDescription}</h1>
+        </div>
+        <div className="product_description">
+          <h1 className="product_description">product_origfile_blob 서버: {productDetails[0].productOrigFile}</h1>
+        </div>
+      </div>
+      </>
+    )
 }
 
 export default StoreDetailsSelection;
