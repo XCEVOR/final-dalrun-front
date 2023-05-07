3fc80c0aa4eb0efc1b220f951e2e75778b29791f
AnSeonJeong <asj081@naver.com>
2023-05-07 19:48:26 +0900

    feat : 관리자 대시보드 Task UI & link 연결

--- src/views/inner-pages/dalrun-asj/inner/AdminDashboard.jsx
+++ src/views/inner-pages/dalrun-asj/inner/AdminDashboard.jsx
@@ -1,10 +1,13 @@
+import TaskList from "../../../../components/dalrun-asj/dashboard/TaskList";
+
 function AdminDashboard() {
   return (
     <div className="dashboard container">
       <span className="title">대시보드</span>
       <div className="dashboard-content">
-        <div className="todolist outline">
+        <div className="todolist">
           <span className="subtitle">오늘 할 일</span>
+          <TaskList />
         </div>
         <div className="summary">
           <div className="visiter outline">
