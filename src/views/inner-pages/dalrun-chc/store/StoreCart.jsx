a1ef0824a2bef6c8af668697a1ac6db7ca979cba
XCEVOR <XCEVOR@GMAIL.COM>
2023-05-10 15:01:08 +0900

    docs: 코드 정리중 (스토어 카트)

--- src/views/inner-pages/dalrun-chc/store/StoreCart.jsx
+++ src/views/inner-pages/dalrun-chc/store/StoreCart.jsx
@@ -35,19 +35,15 @@ function StoreCart() {
             
 
 
-
             <section>
               <div className="ptf-single-post__wrapper">
                 <div className="container-xxl">
                   <div className="row">
-                  <StoreCartList />
+                    <StoreCartList />
                   </div>
                 </div>
               </div>
             </section>
-
-
-
 
 
             
@@ -55,72 +51,6 @@ function StoreCart() {
           {/* End .ptf-page */}
         </div>
 
-
-
-
-      <div className="ptf-main">
-        <div className="ptf-page ptf-page--contact">
-          <section>
-            {/* <!--Spacer--> */}
-            <div
-              className="ptf-spacer"
-              style={{ "--ptf-xxl": "10rem", "--ptf-md": "5rem" }}
-            ></div>
-<StoreCartList />
-            <div className="container-xxl">
-              <div className="row">
-                <div className="col-xl-12">
-                  {/* <!--Animated Block--> */}
-                  <div
-                    className="ptf-animated-block"
-                    data-aos="fade"
-                    data-aos-delay="0"
-                  >
-                    <h1 className="large-heading">
-                      STORE <br />
-                      
-                    </h1>
-                    {/* <!--Spacer--> */}
-                    <div
-                      className="ptf-spacer"
-                      style={{ "--ptf-xxl": "3rem", "--ptf-md": "2.5rem" }}
-                    ></div>
-
-
-
-                    <div
-                      className="ptf-animated-block"
-                      data-aos="fade"
-                      data-aos-delay="0"
-                    >
-                      <StoreCartList />
-                    </div>
-
-
-
-                    <Social />
-                    {/* <!--Social Icon--> */}
-                  </div>
-                </div>
-              </div>
-              {/* End .row */}
-            </div>
-            {/* Contact top portion */}
-
-            {/* <!--Spacer--> */}
-            <div
-              className="ptf-spacer"
-              style={{ "--ptf-xxl": "6.25rem", "--ptf-md": "3.125rem" }}
-            ></div>
-
-
-
-          </section>
-          {/* End section contact header */}
-
-
-        </div>
-      </div>
 
       {/*=============================================
         Start Footer
@@ -143,7 +73,121 @@ function StoreCart() {
     // DEVELOPER_MODE @@@@@ @@@@@ @@@@@ @@@@@ @@@@@ DEVELOPER_MODE @@@@@ @@@@@ @@@@@ @@@@@ @@@@@ DEVELOPER_MODE @@@@@ @@@@@ @@@@@ @@@@@ @@@@@ DEVELOPER_MODE @@@@@ @@@@@ @@@@@ @@@@@ @@@@@ 
     : (
       <>    <input type='checkbox' onClick={() => (setCheckbox_DisplayMode(!checkbox_DisplayMode))}/>DEVELOPER_MODE
+      <div className="dalrun_hc">
+        <Helmet>
+          <title>Moonex - Contact</title>
+        </Helmet>
+        {/* End Page SEO Content */}
 
+        <HeaderDefault />
+        {/* End Header */}
+
+        <div className="main">
+            <article className="ptf-page ptf-page--single-work-1">
+              
+
+
+
+              <section>
+                <div className="ptf-single-post__wrapper">
+                  <div className="container-xxl">
+                    <div className="row">
+                    <StoreCartList />
+                    </div>
+                  </div>
+                </div>
+              </section>
+
+
+
+
+
+              
+            </article>
+            {/* End .ptf-page */}
+          </div>
+
+
+
+
+        <div className="ptf-main">
+          <div className="ptf-page ptf-page--contact">
+            <section>
+              {/* <!--Spacer--> */}
+              <div
+                className="ptf-spacer"
+                style={{ "--ptf-xxl": "10rem", "--ptf-md": "5rem" }}
+              ></div>
+          <StoreCartList />
+              <div className="container-xxl">
+                <div className="row">
+                  <div className="col-xl-12">
+                    {/* <!--Animated Block--> */}
+                    <div
+                      className="ptf-animated-block"
+                      data-aos="fade"
+                      data-aos-delay="0"
+                    >
+                      <h1 className="large-heading">
+                        STORE <br />
+                        
+                      </h1>
+                      {/* <!--Spacer--> */}
+                      <div
+                        className="ptf-spacer"
+                        style={{ "--ptf-xxl": "3rem", "--ptf-md": "2.5rem" }}
+                      ></div>
+
+
+
+                      <div
+                        className="ptf-animated-block"
+                        data-aos="fade"
+                        data-aos-delay="0"
+                      >
+                        <StoreCartList />
+                      </div>
+
+
+
+                      <Social />
+                      {/* <!--Social Icon--> */}
+                    </div>
+                  </div>
+                </div>
+                {/* End .row */}
+              </div>
+              {/* Contact top portion */}
+
+              {/* <!--Spacer--> */}
+              <div
+                className="ptf-spacer"
+                style={{ "--ptf-xxl": "6.25rem", "--ptf-md": "3.125rem" }}
+              ></div>
+
+
+
+            </section>
+            {/* End section contact header */}
+
+
+          </div>
+        </div>
+
+        {/*=============================================
+          Start Footer
+          ============================================== */}
+        <footer className="ptf-footer ptf-footer--style-1">
+          <div className="container-xxl">
+            <div className="ptf-footer__top">
+              <Footer />
+            </div>
+            <div className="ptf-footer__bottom">
+              <CopyRight />
+            </div>
+          </div>
+        </footer>
+      </div>
       </>
     )
 }
