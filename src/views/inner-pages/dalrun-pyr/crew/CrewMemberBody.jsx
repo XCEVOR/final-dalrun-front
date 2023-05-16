5552d740943e6e7e370e01b153731a7550ed0f3e
AnSeonJeong <asj081@naver.com>
2023-05-16 19:19:40 +0900

    feat : 크루 소개 데이터 렌더링

--- src/views/inner-pages/dalrun-pyr/crew/CrewMemberBody.jsx
+++ src/views/inner-pages/dalrun-pyr/crew/CrewMemberBody.jsx
@@ -26,6 +26,12 @@ function CrewMemberBody() {
     history("crewMemberWait", { state:{'crewSeq':crewSeq} });
   }
 
+  const mycrewMemberList = (seq) => {
+    axios.get("http://localhost:3000/mycrewMemberList", { params:{'crewSeq': seq} })
+    .then((resp) => setDataList(resp.data))
+    .catch((err) => alert(err));
+  }
+
   const getLeader = (seq) => {
     axios.get("http://localhost:3000/getLeader", { params:{'crewSeq': seq} })
       .then((resp) => setLeader(resp.data))
@@ -33,23 +39,38 @@ function CrewMemberBody() {
   }
 
   useEffect(() => {
+    mycrewMemberList(crewSeq);
     getLeader(crewSeq);
   }, []);
   
   return (
     
-    <div>
+    <div className="crewmem_introduce">
       <button>크루멤버 소개</button>
       {login.memId === leader ? <button onClick={gotoMemberWait}>크루멤버 대기</button> : ''}
       <table>
         <thead>
           <tr>
-            <th>프로필사진</th>
+            <th style={{width:'300px'}}>프로필사진</th>
             <th>아이디</th>
             <th>등급</th>
+            <th>권한</th>
           </tr>
         </thead>
-       
+        <tbody>
+          {
+            dataList.map((crewmem, i) => {
+              return(
+                <tr key={i}>
+                  <td><img src={`http://localhost:3000/dalrun-yr/profiles/${crewmem.profile}`} /></td>
+                  <td>{crewmem.memId}</td>
+                  <td>{crewmem.grade}</td>
+                  <td>{crewmem.memId === leader ? "리더":""}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
       </table>
     </div>
   );
