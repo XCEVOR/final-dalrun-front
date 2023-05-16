30f44b9f94d2031063bc92e79eca6b1337e27c64
AnSeonJeong <asj081@naver.com>
2023-05-16 10:45:27 +0900

    fix : 로그인, 로그아웃 버튼 수정

--- src/components/dalrun-jy/menu/LoginFormMenu.jsx
+++ src/components/dalrun-jy/menu/LoginFormMenu.jsx
@@ -5,44 +5,35 @@ import { Link } from "react-router-dom";
 
 
 const LoginFormMenu = () => {
+  const logindata=JSON.parse(localStorage.getItem('login'));
 
-  const [login,setLogin]=useState([]);
-  
-  function loading(){
-    const logindata=JSON.parse(localStorage.getItem('login'));
-    console.log(logindata)
-    if(logindata){
-      setLogin(logindata);
-      
-    }
+  function logout() {
+    localStorage.removeItem('login');
+    localStorage.removeItem('memId');
   }
-  useEffect(() => {
 
-    //localStorage.removeItem('login');
-    loading();
+  useEffect(() => {
+    console.log(logindata);
   
-  }, []);
+  }, [logindata]);
 
   return (
     <nav className="ptf-nav">
       {/* <!--Menu--> */}
       <ul className="sf-menu">
 
-        { login.length>0 &&
         <li>
+        { logindata === null ?
           <Link to='/login'>
             <span>로그인</span>
-          </Link>
-        </li>
-        ||
-        <li>
-          <Link to='/login'>
+          </Link> :
+          <Link onClick={logout}>
             <span>로그아웃</span>
           </Link>
+          }
         </li>
-       
-        }
-
+        <li>
+        </li>
       </ul>
     </nav>
   );
