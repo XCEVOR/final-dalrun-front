37a00b113e7dcc15aaf122e817b0c973209c19e1
AnSeonJeong <asj081@naver.com>
2023-05-18 09:23:56 +0900

    fix : 다이어리 업데이트 수정 & CustomEditor 추가

---
+++ src/components/dalrun-asj/CustomEditor.jsx
@@ -0,0 +1,111 @@
+import React, { useState, useRef, useMemo } from 'react';
+import axios from 'axios';
+import ReactQuill from 'react-quill';
+import { nanoid } from '@reduxjs/toolkit';
+import 'react-quill/dist/quill.snow.css';
+import { useEffect } from 'react';
+
+const CustomEditor = ({ handleEditorChange, val, update, updatedFiles, id }) => {
+  const [value, setValue] = useState('');
+  const quillRef = useRef();
+  const loginData = JSON.parse(localStorage.getItem("login"));
+  const postId = nanoid();
+
+  console.log(val);
+  let memId = null;
+  if (loginData) {
+    memId = loginData.memId;
+  }
+
+
+  const deleteImg = () => {
+    console.log(updatedFiles);
+    let formData = new FormData();
+    formData.append('fileList', updatedFiles);
+    formData.append('memId', id);
+
+    axios.post('http://localhost:3000/deleteImg', formData)
+      .then((resp) => console.log("delete images"))
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    console.log(memId)
+    console.log("updatedFiles =", updatedFiles);
+    if(update === true) deleteImg();
+  }, [update]);
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ 'header': ['1', '2', '3', '4', '5', '6', false] }],
+          ['bold', 'italic', 'underline', 'strike', 'clear'],
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          [{ 'indent': '-1'}, { 'indent': '+1' }],
+          [{ 'size': ['small', false, 'large', 'huge'] }],
+          [{ 'font': [] }],
+          [{ 'color': [] }, { 'background': [] }],
+          [{ 'align': [] }],
+          ['link', 'image'],
+        ],
+        handlers: {
+          image: () => {
+            let fileInput = document.createElement('input');
+            fileInput.setAttribute('type', 'file');
+            fileInput.setAttribute('accept', 'image/*');
+            fileInput.click();
+            
+            fileInput.onchange = async () => {
+              let file = fileInput.files[0];
+              
+              let formData = new FormData();
+              formData.append('imageFile', file);
+              id !== undefined ? formData.append('memId', memId) : formData.append('memId', id);
+              formData.append('postId', postId);
+
+              try {
+                const response = await axios.post('http://localhost:3000/uploadDiaryImg', formData);
+                const range = quillRef.current.getEditor().getSelection();
+                quillRef.current.getEditor().insertEmbed(range.index, 'image', response.data.url);
+              } catch (error) {
+                console.error('Error during image upload:', error);
+              }
+            };
+          },
+        },
+      },
+    };
+  }, []);
+
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'clear',
+    'list', 'bullet', 'indent',
+    'size', 'font',
+    'color', 'background',
+    'align',
+    'link', 'image'
+  ];
+
+  const handleChange = (content) => {
+    setValue(content);
+    handleEditorChange({ target: { name: 'content', value: content } });
+  };
+
+  return (
+    <div style={{ height: 500, width: 1000, overflow: 'auto' }}>
+      <ReactQuill 
+        ref={quillRef} 
+        theme="snow" 
+        modules={modules}
+        formats={formats}
+        value={val !== undefined ? val : value} 
+        onChange={handleChange} 
+        style={{ marginTop:'1rem', height: 430, width: 970 }}
+      />
+    </div>
+  );
+};
+
+export default CustomEditor;
